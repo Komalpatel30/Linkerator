ac607b7d7fc9db8e85589d3135752cf623cf16bc
Komal <komal.mpatel30@gmail.com>
2021-04-22 22:42:21 -0400

    build added

---
+++ build/precache-manifest.c7d9dc71c139c31e4c713976754989ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7399459a47ad0472bd783f46fe55ee6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58b2f3785040cec81278",
+    "url": "/static/css/2.de5665f9.chunk.css"
+  },
+  {
+    "revision": "58b2f3785040cec81278",
+    "url": "/static/js/2.a6ed9081.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a6ed9081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e40e73455c641f306a1b",
+    "url": "/static/js/main.a790ba92.chunk.js"
+  },
+  {
+    "revision": "96bf99c166ab513bf2df",
+    "url": "/static/js/runtime-main.0cd45702.js"
+  }
+]);
